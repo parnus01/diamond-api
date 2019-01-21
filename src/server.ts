afaf98f3a9dd7a204f762d65908952903a7f34b5
Danusorn Taboonyong <danusorn.t@acommerce.asia>
2019-01-21 16:41:07 +0700

    fix port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app/app';
 import databaseConnection from './database/database.connection';
 // Process.env will always be comprised of strings, so we typecast the port to a
 // number.
-const PORT:number = Number(process.env.PORT) || 3000;
+const PORT:number = Number(process.env.PORT) || 8080;
 
 databaseConnection
   .then(() => app.listen(PORT))
