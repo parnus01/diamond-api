443c578528cffc8356b0853bb9ddb244339dde52
Danusorn Taboonyong <danusorn.t@ku.th>
2019-01-21 14:52:59 +0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
 module.exports = {
   test: {
     client: 'pg',
-    connection: 'postgres://postgres:postgres@localhost:5432/koa_api_test',
+    connection: 'postgres://cymyvlgwxhslqc:6bd1917d4cedd88937be3812f3495ee452aaafe7a3f6dfa77c9cd1c98b4ae7b7@ec2-107-22-162-8.compute-1.amazonaws.com:5432/d5d5rmet15kpv0',
     migrations: {
       directory: path.join(BASE_PATH, 'migrations')
     },
@@ -15,7 +15,7 @@ module.exports = {
   },
   development: {
     client: 'pg',
-    connection: 'postgres://postgres:postgres@localhost:5432/koa_api',
+    connection: 'postgres://cymyvlgwxhslqc:6bd1917d4cedd88937be3812f3495ee452aaafe7a3f6dfa77c9cd1c98b4ae7b7@ec2-107-22-162-8.compute-1.amazonaws.com:5432/d5d5rmet15kpv0',
     migrations: {
       directory: path.join(BASE_PATH, 'migrations')
     },
